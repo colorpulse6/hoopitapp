672303b8202dd5bfc38ede4271adae5e1dc5c333
Nic Barnes <colorpulse@gmail.com>
2020-07-07 19:28:25 +0200

    fixed reroute bug usermain

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -15,7 +15,7 @@ export default function Nav(props) {
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
-                        <Link to={props.loggedInUser ? "user-main":"/"} ><img src={ bball } width="30" height="30" class="d-inline-block align-top bball-nav" alt=""></img></Link>
+                        <Link to={props.loggedInUser ? "/user-main":"/"} ><img src={ bball } width="30" height="30" class="d-inline-block align-top bball-nav" alt=""></img></Link>
                         <p className="hoopItApp">hoop.it.App</p>
                         
                     </div>
